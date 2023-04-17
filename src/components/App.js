ffd607fb6aa4f7d1d3d238ccdfca53963ddcc068
manojr123 <manojraghavan2008@yahoo.co.in>
2023-04-17 17:24:35 +0530

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import { useEffect, useState } from 'react';
-
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 import { getPosts } from '../api';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+
 // import { Loader } from './';
 // import App from './App';
 import Loader from './Loader';
 import Navbar from './Navbar'
+
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
+
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -31,11 +47,19 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
-      <h1>App</h1>
-      <Home posts={posts} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route  path="/" element={<Home posts={posts} />} />
+          <Route  path="/login" element={<Login />} />
+          <Route  path="/about" element={<About />} />
+          <Route  path="/userinfo" element={<UserInfo />} />
+          <Route  path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+
     </div>
-  );
+  )
 }
 
 export default App;
