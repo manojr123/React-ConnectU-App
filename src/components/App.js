de726f96a444ea18343bc324c86387ec689fa3fb
manojr123 <manojraghavan2008@yahoo.co.in>
2023-04-17 15:51:50 +0530

    Adding code for Home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
+
+import { getPosts } from '../api';
+import Home from '../pages/Home';
+// import { Loader } from './';
+// import App from './App';
+import Loader from './Loader';
+import Navbar from './Navbar'
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <h1> Codeial</h1>
+      <Navbar />
+      <h1>App</h1>
+      <Home posts={posts} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("in index.js");
 root.render(
   <React.StrictMode>
     <App />
